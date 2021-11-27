760bb3e4ce8de548046516b279522a98f43e1065
Gbenro Adesoye <Gbenro_Adesoye@yahoo.com>
2021-11-26 23:28:46 -0600

    changed text color

--- src/App.js
+++ src/App.js
@@ -385,7 +385,7 @@ function App() {
           <s.TextDescription
             style={{
               textAlign: "center",
-              color: "var(--secondary-text)",
+              color: "purple",
             }}
           >
             Please make sure you are connected to the right network (
@@ -396,7 +396,7 @@ function App() {
           <s.TextDescription
             style={{
               textAlign: "center",
-              color: "var(--primary-text)",
+              color: "purple",
             }}
           >
             We have set the gas limit to {CONFIG.GAS_LIMIT} for the contract to
